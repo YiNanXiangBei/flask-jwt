e64a18f07bdbcf9383a9d1cce7e3e598fa7439a7
Lao wang <vipkia@sina.cn>
2018-06-27 20:19:46 +0800

    更新

--- application/configs.py
+++ application/configs.py
@@ -1,6 +1,6 @@
 from datetime import timedelta
 
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:ws9949021044@127.0.0.1:3306/flask_jwt"
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:root@127.0.0.1:3306/flask_jwt"
 SQLALCHEMY_TRACK_MODIFICATIONS = True
 jwt_config = {
     'JWT_SECRET_KEY': 'jwt_secret',
